6709e178a0b5ba35d5419888baddb215a8501174
jrlamb <julianolamb@gmail.com>
2014-12-29 23:58:56 -0200

    First commit - class Person was added

---
+++ src/main/java/br/com/company/model/Person.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.company.model;
+
+/**
+ *
+ * @author Juliano
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private char gender;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    
+    
+    
+}
